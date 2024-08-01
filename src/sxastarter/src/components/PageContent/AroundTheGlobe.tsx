3c4756f6adbc3d4e08c3e3b2da87a25d6f6fef61
Chris Haddad <cha@sitecore.net>
2024-08-01 01:34:43 -0500

    updated images to public links from CH

--- src/sxastarter/src/components/PageContent/AroundTheGlobe.tsx
+++ src/sxastarter/src/components/PageContent/AroundTheGlobe.tsx
@@ -27,92 +27,92 @@ export const Default = (): JSX.Element => {
                             <div className="row row-cols-2 row-cols-lg-3 g-3 js-filter-by-class-parent">
                                 <div className="col">
                                     <a href="https://www.kelloggs.com/en_US/brands/austin.html" target="_blank" className="product-tile js-filter-item js-filter-item_UnitedStates">
-                                        <img src="https://www.kellanova.com/content/dam/global/kellanova-company/images/our-food/us/austin%402x.png" alt="Austin" className="lazyload" />
+                                        <img src="https://kellanova.sitecoresandbox.cloud/api/public/content/df5c7c1f74604e05bf98781b689c8971?v=b1fb810e" alt="Austin" className="lazyload" />
                                     </a>
                                 </div>
                                 <div className="col">
                                     <a href="https://www.carrscrackers.com" target="_blank" className="product-tile js-filter-item js-filter-item_UnitedStates">
-                                        <img src="https://www.kellanova.com/content/dam/NorthAmerica/kellanova-us/images/ourbrands/logos/carrsnew.png" alt="Carr's" className="lazyload" />
+                                        <img src="https://kellanova.sitecoresandbox.cloud/api/public/content/2e33e665f0704425b97112f0a695fd93?v=ccc9c12d" alt="Carr's" className="lazyload" />
                                     </a>
                                 </div>
                                 <div className="col">
                                     <a href="https://www.cheezit.com/en_US/home.html" target="_blank" className="product-tile js-filter-item js-filter-item_UnitedStates">
-                                        <img src="https://www.kellanova.com/content/dam/global/kellanova-company/images/our-food/us/cheezit@2x.png" alt="Cheez-It" className="lazyload" />
+                                        <img src="https://kellanova.sitecoresandbox.cloud/api/public/content/855ed6c8fecd4200bed591cdf1966bc5?v=f85a9366" alt="Cheez-It" className="lazyload" />
                                     </a>
                                 </div>
                                 <div className="col">
                                     <a href="https://www.clubcrackers.com/en_US/home.html" target="_blank" className="product-tile js-filter-item js-filter-item_UnitedStates">
-                                        <img src="https://www.kellanova.com/content/dam/global/kellanova-company/images/our-food/us/club@2x.png" alt="Club" className="lazyload"/>
+                                        <img src="https://kellanova.sitecoresandbox.cloud/api/public/content/b113a1f5b852475ca9f865156f92d539?v=3f6072d4" alt="Club" className="lazyload"/>
                                     </a>
                                 </div>
                                 <div className="col">
                                     <a href="https://www.leggowitheggo.com/en_US/home.html" target="_blank" className="product-tile js-filter-item js-filter-item_UnitedStates">
-                                        <img src="https://www.kellanova.com/content/dam/global/kellanova-company/images/our-food/us/eggo@2x.png" alt="Eggo" className="lazyload"/>
+                                        <img src="https://kellanova.sitecoresandbox.cloud/api/public/content/cca1e59fec954cf8927017bb1a46b2b2?v=a9f4334f" alt="Eggo" className="lazyload"/>
                                     </a>
                                 </div>
                                 <div className="col">
                                     <a href="https://www.kellanovaus.com/us/en/brands/grahams.html" target="_blank" className="product-tile js-filter-item js-filter-item_UnitedStates">
-                                        <img src="https://www.kellanova.com/content/dam/global/kellanova-company/images/our-food/us/grahams@2x.png" alt="Grahams" className="lazyload"/>
+                                        <img src="https://kellanova.sitecoresandbox.cloud/api/public/content/398bac695e144073907c389e702c366c?v=51537617" alt="Grahams" className="lazyload"/>
                                     </a>
                                 </div>
                                 <div className="col">
                                     <a href="https://www.kellanovaus.com/us/en/brands/kelloggs-frozen-breakfast.html" target="_blank" className="product-tile js-filter-item js-filter-item_UnitedStates">
-                                        <img src="https://www.kellanova.com/content/dam/global/kellanova-company/images/our-food/us/frozen%20breakfast@2x.png" alt="Kellogg's Frozen Breakfast" className="lazyload"/>
+                                        <img src="https://kellanova.sitecoresandbox.cloud/api/public/content/cfe76e2fe1e641dd9be0d16d80813e62?v=8403de07" alt="Kellogg's Frozen Breakfast" className="lazyload"/>
                                     </a>
                                 </div>
                                 <div className="col">
                                     <a href="https://www.morningstarfarms.com/en_US/home.html" target="_blank" className="product-tile js-filter-item js-filter-item_UnitedStates">
-                                        <img src="https://www.kellanova.com/content/dam/global/kellanova-company/images/our-food/us/msf@2x.png" alt="Morningstar Farms" className="lazyload"/>
+                                        <img src="https://kellanova.sitecoresandbox.cloud/api/public/content/40bd49cd88ce4b1c9eb58a33ce318f0e?v=730ef2ba" alt="Morningstar Farms" className="lazyload"/>
                                     </a>
                                 </div>
                                 <div className="col">
                                     <a href="https://www.nutrigrain.com/en_US/home.html" target="_blank" className="product-tile js-filter-item js-filter-item_UnitedStates">
-                                        <img src="https://www.kellanova.com/content/dam/global/kellanova-company/images/our-food/us/ng@2x.png" alt="Nutri-Grain" className="lazyload"/>
+                                        <img src="https://kellanova.sitecoresandbox.cloud/api/public/content/73776763ad1c40f3a62f69a35e2ffec2?v=af28bdb6" alt="Nutri-Grain" className="lazyload"/>
                                     </a>
                                 </div>
                                 <div className="col">
                                     <a href="https://www.poptarts.com/en_US/home.html" target="_blank" className="product-tile js-filter-item js-filter-item_UnitedStates">
-                                        <img src="https://www.kellanova.com/content/dam/global/kellanova-company/images/our-food/us/pt@2x.png" alt="Pop-Tarts" className="lazyload"/>
+                                        <img src="https://kellanova.sitecoresandbox.cloud/api/public/content/8de2e300c20a4a09889ee899a828af7e?v=e70c03dc" alt="Pop-Tarts" className="lazyload"/>
                                     </a>
                                 </div>
                                 <div className="col">
                                     <a href="https://www.pringles.com/us/home.html" target="_blank" className="product-tile js-filter-item js-filter-item_UnitedStates">
-                                        <img src="https://www.kellanova.com/content/dam/global/kellanova-company/images/our-food/us/pringles@2x.png" alt="Pringles" className="lazyload"/>
+                                        <img src="https://kellanova.sitecoresandbox.cloud/api/public/content/941115a79fe24731a8e440ae1098a763?v=cf198dcf" alt="Pringles" className="lazyload"/>
                                     </a>
                                 </div>
                                 <div className="col">
                                     <a href="https://www.pureorganic.com/en_US/home.html" target="_blank" className="product-tile js-filter-item js-filter-item_UnitedStates">
-                                        <img src="https://www.kellanova.com/content/dam/global/kellanova-company/images/our-food/us/pure%20organic@2x.png" alt="Pure Organic" className="lazyload"/>
+                                        <img src="https://kellanova.sitecoresandbox.cloud/api/public/content/84acb7cb0c1044879b0d66b4d62e625c?v=5dec310d" alt="Pure Organic" className="lazyload"/>
                                     </a>
                                 </div>
                                 <div className="col">
                                     <a href="https://www.ricekrispiestreats.com/en-us/home.html" target="_blank" className="product-tile js-filter-item js-filter-item_UnitedStates">
-                                        <img src="https://www.kellanova.com/content/dam/global/kellanova-company/images/our-food/us/rkt%402x.png" alt="Rice Krispies Treats" className="lazyload"/>
+                                        <img src="https://kellanova.sitecoresandbox.cloud/api/public/content/e6396b8a311c414082e87c5b939c87fa?v=3df8e7a4" alt="Rice Krispies Treats" className="lazyload"/>
                                     </a>
                                 </div>
                                 <div className="col">
                                     <a href="https://www.rxbar.com/en_US/home.html" target="_blank" className="product-tile js-filter-item js-filter-item_UnitedStates">
-                                        <img src="https://www.kellanova.com/content/dam/global/kellanova-company/images/our-food/ca/rx@2x.png" alt="RxBar" className="lazyload"/>
+                                        <img src="https://kellanova.sitecoresandbox.cloud/api/public/content/351068f6030a4c69a336e0cb3258c096?v=e754208e" alt="RxBar" className="lazyload"/>
                                     </a>
                                 </div>
                                 <div className="col">
                                     <a href="https://www.specialk.com/en_US/home.html" target="_blank" className="product-tile js-filter-item js-filter-item_UnitedStates">
-                                        <img src="https://www.kellanova.com/content/dam/global/kellanova-company/images/our-food/us/special%20k@2x.png" alt="Special K" className="lazyload"/>
+                                        <img src="https://kellanova.sitecoresandbox.cloud/api/public/content/07652cc77682422eaaebd815d47228cc?v=2051ed5e" alt="Special K" className="lazyload"/>
                                     </a>
                                 </div>
                                 <div className="col">
                                     <a href="https://www.kelloggs.com/en_US/brands/toasteds-consumer-brand.html" target="_blank" className="product-tile js-filter-item js-filter-item_UnitedStates">
-                                        <img src="https://www.kellanova.com/content/dam/global/kellanova-company/images/around-the-world/toastedsnew.png" alt="Toasteds" className="lazyload"/>
+                                        <img src="https://kellanova.sitecoresandbox.cloud/api/public/content/12fd211697d94bf8ac5ee8f094fe4166?v=acb43c1c" alt="Toasteds" className="lazyload"/>
                                     </a>
                                 </div>
                                 <div className="col">
                                     <a href="https://www.townhousecrackers.com/en_US/home.html" target="_blank" className="product-tile js-filter-item js-filter-item_UnitedStates">
-                                        <img src="https://www.kellanova.com/content/dam/global/kellanova-company/images/around-the-world/townhousenew.png" alt="Town House" className="lazyload"/>
+                                        <img src="https://kellanova.sitecoresandbox.cloud/api/public/content/6c0958fd8aeb47bab21e3ffca160cfec?v=67f77696" alt="Town House" className="lazyload"/>
                                     </a>
                                 </div>
                                 <div className="col">
                                     <a href="https://www.kelloggs.com/en_US/brands/zesta.html" target="_blank" className="product-tile js-filter-item js-filter-item_UnitedStates">
-                                        <img src="https://www.kellanova.com/content/dam/global/kellanova-company/images/our-food/us/zesta@2x.png" alt="Zesta" className="lazyload"/>
+                                        <img src="https://kellanova.sitecoresandbox.cloud/api/public/content/8d16f7551b574bfc8609387fe4ec1b82?v=189ec778" alt="Zesta" className="lazyload"/>
                                     </a>
                                 </div>
                             </div>
